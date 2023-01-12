241ffcf2c008d38354a3244effbfd6a649de2d01
pratham-0094 <sahupratham022003@gmail.com>
2023-01-12 23:49:26 +0530

    Day-003

--- Day-003/time_space_complexity.cpp
+++ Day-003/time_space_complexity.cpp
@@ -1,4 +1,43 @@
-// time and space complexity
+// time complexity --> the amount of time required for performing computation task
+// depends of procedure that we are adopting
 
-// time complexity - time required to execute a program
-// space complexity - storage required by the program at any point
+// space completxity --> how much space is consumed in main memory during the execution of a program
+
+void swap(int x, int y)
+{
+    int t;
+    t = x; // -------- 1
+    x = y; // -------- 1
+    y = t; // -------- 1
+
+    // f(n) = 3 n ^ 0
+    // O(n ^ 0) = O(n)
+}
+
+int sum(int A[], int n)
+{
+    int s = 0;                  // --------------------- 1
+    for (int i = 0; i < n; i++) // --------------------- n + 1
+    {
+        s = s + A[i]; // --------------------- n
+    }
+    return s;
+
+    // f(n) = 2 n ^ 1 + 3
+    // O(n)
+}
+
+void add(int n)
+{
+    for (int i = i; i < n; i++) // --------------------- n + 1
+    {
+
+        for (int j = 0; j < n; j++) // --------------------- n * (n + 1)
+        {
+            // C[i][j] = A[i][j] + B[i][j]; // --------------------- n * n
+        }
+    }
+
+    // f(n) = 2 n ^ 2 + 2 n + 1
+    // O(n) = n  ^ 2
+}
